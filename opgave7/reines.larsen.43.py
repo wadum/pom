049bd41faff507342157eb512cce5ed217501154
Toke Emil Heldbo Reines <toke@heldbo.net>
2014-10-31 10:51:54 +0100

    opg7

--- opgave7/reines.larsen.43.py
+++ opgave7/reines.larsen.43.py
@@ -9,6 +9,15 @@ import pylab
 import numpy
 
 def rInt(f, a, b, n):
+	u''' Beregner Riemannsummen givet funktionen f, et start punkt, a, et slutpunkt, b,
+		og et antal knudepunkter n
+		args:
+			f - funktion
+			a - startpunk
+			b - slutpunkt
+			n - antal equidistante knudepunkter 
+		return:
+			Riemannsummen, summen af arealer på rektangler for hvert interval, 1 ... n'''
 	n_list = partition(a, b, n)
 	n_list.reverse()
 	ti = n_list.pop()
@@ -22,6 +31,15 @@ def rInt(f, a, b, n):
 	return res
 
 def rIntMid(f, a, b, n):
+	u''' Beregner Riemannsummen givet funktionen f, et start punkt, a, et slutpunkt, b,
+		og et antal knudepunkter n
+		args:
+			f - funktion
+			a - startpunk
+			b - slutpunkt
+			n - antal knudepunkter 
+		return:
+			Riemannsummen, middelsummen af arealet på rektangler for hvert interval, 1 ... n'''
 	n_list = partition(a, b, n)
 	n_list.reverse()
 	ti = n_list.pop()
@@ -35,6 +53,7 @@ def rIntMid(f, a, b, n):
 	return res
 
 def partition(a, b, n):
+	u''' Partitionerer intervallet [a ... b] op i n lige store summer '''
 	frc = (b-a)/n
 	lst = [float(a)]
 	for x in range(n):
@@ -43,18 +62,23 @@ def partition(a, b, n):
 
 
 def f(x):
+	''' returnerer Cos(x) '''
 	return numpy.cos(x)
 
 def o(x):
+	''' returnerer 1/sqrt(2pi)*(x^2/2) '''
 	return (1/(numpy.sqrt(2*numpy.pi))*numpy.exp(-1*((x**2)/2)))
 
 def g(x):
+	''' returnerer sin(1/x) '''
 	return numpy.sin(1/x)
 
 def testPartition():
+	''' Tester partition-funktionen '''
 	return partition(0, 10 , 10)  == [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
 
 def testf():
+	''' Tester og plotter f(x) '''
 	pylab.figure()
 	print rInt(f, 0, 10, 10)
 	print rIntMid(f, 0, 10, 10)
@@ -62,6 +86,7 @@ def testf():
 	pylab.plot(x, f(x))
 
 def testo():
+	''' Tester og plotter o(x) '''
 	pylab.figure()
 	print rInt(o, -10, 10, 20)
 	print rIntMid(o, -10, 10, 20)
@@ -69,6 +94,7 @@ def testo():
 	pylab.plot(x, o(x))
 
 def testg():
+	''' Tester og plotter g(x) '''
 	pylab.figure()
 	print rInt(g, 0.001, 10, 10)
 	print rIntMid(g, 0.001, 10, 10)

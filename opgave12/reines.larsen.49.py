9b99396939d7a8fb7875e224dc1e13e9b3938f99
Chris <chrwadum@gmail.com>
2014-12-15 23:48:32 +0100

    docstrings opgave 12

--- opgave12/reines.larsen.49.py
+++ opgave12/reines.larsen.49.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import time
 
 
+# Konstanter
 constant = 1.0
 h = 1.0
 k = 1.0
@@ -18,20 +19,47 @@ def u(x, t):
 	return math.exp(-((x-8-constant*t)**2)/4)
 
 def a(x, t):
+	""" Gruppering a i rapporten.
+
+		return:
+			u(x+h, t)
+	"""
 	return u(x+h, t)
 
 def b(x, t):
+	""" Gruppering b i rapporten.
+
+		return:
+			2 * u(x, t)
+	"""
 	return 2 * u(x, t)
 
 def c(x, t):
+	""" Gruppering c i rapporten.
+
+		return:
+			u(x-h, t)
+	"""
 	return u(x-h, t)
 
 def e(x, t):
+	""" Gruppering d i rapporten.
+
+		return:
+			u(x, t-k)
+	"""
 	return u(x, t-k)
 
 def d(x, t):
+	""" Gruppering e i rapporten.
+
+		return:
+			(constant**2 * k**2 * (a(x,t)+c(x,t)) + b(x,t)*(h**2 - constant**2 * k**2) - e(x,t) * h**2)/h**2
+	"""
 	return (constant**2 * k**2 * (a(x,t)+c(x,t)) + b(x,t)*(h**2 - constant**2 * k**2) - e(x,t) * h**2)/h**2
 
+
+# Main funktion
 if __name__ == '__main__':
 	t = 0.0
 	plt.ion()

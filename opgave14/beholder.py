9e6a7751507914752024f56ebc3bc927b5cee004
Toke Reines <toke@heldbo.net>
2015-01-08 21:25:20 +0100

    ting

--- opgave14/beholder.py
+++ opgave14/beholder.py
@@ -4,3 +4,27 @@
 	Beholder
 """
 from __future__ import division
+import math	
+from vektor import *
+from vektorlib import *
+
+class Beholder(object):
+	def __init__(self, radius, centrumPos, partikler):
+		""" Constructor til Beholder.
+		args:
+			radius: beholderens radius
+			centrumPos: beholderens centrumPos. Default er (0,0)
+			partikler: antal partikler der skal instantieres i beholderen
+		"""
+		self.radius = radius
+		self.centrumPos = centrumPos
+		self.partikler = genererPartikler(partikler)
+		
+	def genererPartikler(self, partikler):
+		""" Generere en mængde partikler i beholderen.
+		args:
+			partikler: Antal af partikler der skal genereres tilfældigt
+		"""
+		
+if __name__ == '__main__':
+	print testAlle()
--- opgave14/partikel.py
+++ opgave14/partikel.py
@@ -4,3 +4,47 @@
 	Partikel
 """
 from __future__ import division
+import math	
+from vektor import *
+from vektorlib import *
+
+class Partikel(object):
+	def __init__(self, positionsVektor, hastighedsVektor):
+		""" Constructor til Partikel.
+
+		"""
+		self.positionsVektor = positionsVektor
+		self.hastighedsVektor = hastighedsVektor
+		self.masse = 10**(-23)
+		
+	def hastighed(self):
+		""" Returnerer partiklens hastighed	"""
+		return len(self.hastighedsVektor)
+	
+	def retning(self):
+		""" Returnerer partiklens retningsvektor """
+		return self.hastighedsVektor;
+		
+	def step(self, deltaT):
+		x = self.positionsVektor["x"]+deltaT*self.hastighedsVektor["x"]
+		y = self.positionsVektor["y"]+deltaT*self.hastighedsVektor["y"]
+		return Vektor(x, y)
+		
+def testHastighed():
+	partikel = Partikel(Vektor(0,0), Vektor(3,4))
+	return partikel.hastighed() is 5
+
+def testStep():
+	partikel = Partikel(Vektor(0,0), Vektor(1,1))
+	return partikel.step(1) == Vektor(1,1)
+	
+def testAlle():
+	if not testHastighed():
+		return "testHastighed mislykkedes"
+	elif not testStep():
+		return "testStep mislykkedes"
+	else:
+		return "Alle tests er vellykket"
+		
+if __name__ == '__main__':
+	print testAlle()

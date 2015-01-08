3ca65350bd9c71978d736995a53de654a9194c0f
Chris <chrwadum@gmail.com>
2015-01-08 23:24:53 +0100

    fÃ¦rdig med plot forhÃ¥benligt

--- opgave14/opgave14.py
+++ opgave14/opgave14.py
@@ -6,11 +6,13 @@
 from __future__ import division
 from beholder import *
 import matplotlib.pyplot as plt
+import time
 
 def tegnPartikel(partikel, plot):
 	plot.plot(partikel.positionsVektor["x"], partikel.positionsVektor["y"], 'ro', color="g")
 
 def tegnBeholder(beholder, plot):
+	plt.cla()
 	fig = plot.gcf()
 	fig.gca().add_artist(plot.Circle(beholder.centrumPos, beholder.radius, color='b', fill=False))
 
@@ -21,8 +23,11 @@ def tegnBeholder(beholder, plot):
 	plot.ylim([x-beholder.radius-1,x+beholder.radius+1])
 	plot.xlim([x-beholder.radius-1,x+beholder.radius+1])
 	plot.axes().set_aspect(1./plot.axes().get_data_ratio())
+	plt.draw()
 
 if __name__ == '__main__':
 	beholder = Beholder(5, (0,0), 1000, 0.5)
-	tegnBeholder(beholder, plt)
-	plt.show()
+	plt.ion()
+	for i in range(5):
+		tegnBeholder(beholder, plt)
+		time.sleep(5000)

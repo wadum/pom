08ceca453aee9565ca5eaea38a68142ee87e5493
Chris <chrwadum@gmail.com>
2015-01-08 20:50:27 +0100

    len(vektor)

--- opgave14/vektor.py
+++ opgave14/vektor.py
@@ -4,6 +4,7 @@
 	Vektor
 """
 from __future__ import division
+import math
 
 class Vektor(object):
 	def __init__(self, x, y):
@@ -16,16 +17,23 @@ class Vektor(object):
 			return self.__y
 		else:
 			raise IndexError("%d is not a valid key" % key)
+	def __len__(self):
+		return math.sqrt(self.__x ** 2 + self.__y ** 2)
 
 def testGetItem():
 	vek = Vektor(1,2)
 	return vek["x"] is 1 and vek["y"] is 2
 
-def testAll():
+def testLen():
+	return len(Vektor(3,4)) is 5
+
+def testAlle():
 	if not testGetItem():
 		return "testGetItem mislykkedes"
-	if testGetItem():
+	if not testLen():
+		return "testGetItem mislykkedes"
+	if testGetItem() and testLen():
 		return "Alle tests er vellykket"
 
 if __name__ == '__main__':
-	print testAll()
+	print testAlle()

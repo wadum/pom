056bcafb9b2876f0df65708e2cb5530e68daccd8
Chris <chrwadum@gmail.com>
2015-01-08 21:30:44 +0100

    vektorlib(dot)

--- opgave14/vektorlib.py
+++ opgave14/vektorlib.py
@@ -13,17 +13,25 @@ def scale(v, s):
 def vec((p1x, p1y), (p2x, p2y)):
 	return Vektor(p2x - p1x, p2y - p1y)
 
+def dot(v1, v2):
+	return v1["x"] * v2["x"] + v1["y"] * v2["y"]
+
 def testScale():
 	return scale(Vektor(1,1), 2) == Vektor(2,2)
 
 def testVec():
 	return vec((0,0), (1,1)) == Vektor(1,1)
 
+def testDot():
+	return dot(Vektor(2,2), Vektor(2,2)) == 8
+
 def testAlle():
 	if not testVec():
 		return "testVec mislykkedes"
 	elif not testScale():
 		return "testScale mislykkedes"
+	elif not testDot():
+		return "testDot mislykkedes"
 	else:
 		return "Alle tests er vellykket"
 
